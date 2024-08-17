d25dc921062da1b3a9f754ed1cb211a27f347fb4
Shehryar_Kiyani <shehryar@xenex-media.com.au>
2024-08-17 12:33:32 +0500

    ThemeProvider Set

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable tailwindcss/classnames-order */
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="h1-bold">Next13</h1>
+      <h1 className="h1-bold">Next134</h1>
     </main>
   );
 }
